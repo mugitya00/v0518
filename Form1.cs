7902d11f4870434cffecf335dd285065d8eded09
岸野 <130636405+mugitya00@users.noreply.github.com>
2023-05-18 16:08:31 +0900

    数値

--- Form1.cs
+++ Form1.cs
@@ -9,25 +9,30 @@ namespace v0518
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //button.Left=0;
-            button1.Left = button1.Left - 10;
-            button1.Top = button1.Top - 10;
+            
+            
 
 
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            button1.Top = button1.Top - 30;
-            button2.Top = button2.Top + 30;
-            button3.Left = button3.Left - 30;
-            button4.Left = button4.Left + 30;
+            button1.Top = button1.Top - 50;
+            button2.Top = button2.Top + 50;
+            button3.Left = button3.Left - 50;
+            button4.Left = button4.Left + 50;
             button1.BackColor = Color.Aqua;
             button2.BackColor = Color.DarkViolet;
             button3.BackColor = Color.Red;
             button4.BackColor = Color.Green;
             button5.Visible = false;
             button6.Visible = true;
+            button1.Text = "Ç»Ç∫âüÇµÇΩÅ`";
+            button2.Text = "Ç»Ç∫âüÇµÇΩÅ`";
+            button3.Text = "Ç»Ç∫âüÇµÇΩÅ`";
+            button4.Text = "Ç»Ç∫âüÇµÇΩÅ`";
+            
+
         }
     }
 }
